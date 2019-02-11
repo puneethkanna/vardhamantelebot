0e5070b84c097b356138119a6d75e8b5bf9baf6c
Neerukonda Puneeth <40000406+puneethkanna@users.noreply.github.com>
2019-02-11 10:03:55 +0530

    updated gpa

--- marks.py
+++ marks.py
@@ -28,7 +28,10 @@ def cgpa(rno,pas):
 	th=br.select("th")#3
 	td=br.select("td")#8
 	c=str(br.select)
-	return(str(th[1].text.strip())+":"+str(td[7].text.strip())+"\n"+str(th[24].text.strip()))
+	try:
+		return(str(th[1].text.strip())+":"+str(td[7].text.strip())+"\n"+str(th[24].text.strip()))
+	except IndexError:
+				return("Something went wrong send the report to vardhamanassistant@gmail.com stating the issue. with your rollno")
 def sgpa(rno,pas,semid):
 	br = RoboBrowser(history=True, parser="html.parser")
 	br = RoboBrowser(user_agent='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6')
@@ -44,5 +47,8 @@ def sgpa(rno,pas,semid):
 	c=str(br.select)
 	index={"1":"13","2":"17","3":"21","4":"25","5":"29","6":"33","7":"37","8":"41"}
 	i=int(index[semid])
-	return(str(th[1].text.strip())+":"+str(td[7].text.strip())+"\n"+str(th[i].text.strip()))
+	try:
+		return(str(th[1].text.strip())+":"+str(td[7].text.strip())+"\n"+str(th[i].text.strip()))
+	except IndexError:
+				return("I think you have not completed that semester.\nIf you have completed send your rollno to vardhamanassistant@gmail.com stating the issue.")
 
